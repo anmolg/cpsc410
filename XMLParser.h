874d629c0e25ad92522187255db149e597d96c9f
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2013-10-25 17:46:09 -0700

    Added Model classes and xml parser

---
+++ XMLParser.h
@@ -0,0 +1,22 @@
+
+
+#ifndef _XMLPARSER_
+#define _XMLPARSER_
+
+#include "Model.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class XMLParser {
+
+public:
+	XMLParser() {};
+	~XMLParser() {};
+
+	// parse a file and write the output in the vectors
+	bool parse(	const char *filename);
+};
+
+#endif
