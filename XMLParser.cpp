4d5d1bb65fd3fdb11d1c01a3f2ad0f0f562142ca
dangeroustuna <gaccount@126.com>
2013-10-29 16:57:11 -0700

    parser: for interger and a flag trigger for elements

--- XMLParser.cpp
+++ XMLParser.cpp
@@ -1,13 +1,19 @@
 #include "XMLParser.h"
-
+//#include <boost/tokenizer.hpp>
 using namespace std;
 
-string getNumber(string str, string keyword)
+
+
+/*helper function to prase a line of string into number, if the keyword matches
+return -1 if fail to prase
+str: input line of XML file
+*/
+int getNumber(string str, string keyword)
 {
     string new_str;
     size_t index = 0;
 	int numLength =0;
-
+	int result =-1;
     while (index < str.size())
     {
         if (isspace(str[index]))
@@ -21,8 +27,8 @@ string getNumber(string str, string keyword)
 //compare keyword
 		if (index+keyword.size()+1<str.size()){
 			if(str.substr(index+1,keyword.size())==keyword){
-				cout <<  index << endl;
-				cout << str.size() << endl;
+	//			cout <<  index << endl;
+	//			cout << str.size() << endl;
 				while (numLength+index+2*keyword.size()+5 < str.size())
 				{
 
@@ -34,36 +40,50 @@ string getNumber(string str, string keyword)
 					else
 						break;
 				}
-
+				new_str=str.substr(index+2+keyword.size(),numLength+1);
+	//			cout<<new_str<<endl;  //search for keyword
+				result =atoi(new_str.c_str());
+//				cout<<result<<endl;
 			}
 		}
 
 	
-	if (index < str.size())
-        new_str = str.substr(index);
 
-    return new_str;
+
+//		cout<< "yoHO"<<result+100.0<<endl;
+    return result;
 }
+/*triger a flag for isReading an element of XML file ,for example
+<FILE>
+	...
+</FILE>
+1:start 
+0:in progress
+-1:end
 
-/*bool XMLParser::parse(const char *filename, std::vector<GVersion> *versions) 
-{
-	ifstream in;
-	in.open(filename, ios::in);
-	if(!in.is_open()) {
-		cout << "Error: XMLParser - could not open file " << filename << endl; 
-		return false;
-	}
-
-	char currentLine[256];
-	while(in.getline(currentLine,256)){
-		string line(currentLine);
-		cout << line << endl;
-	}
-}
 */
+int flagTrigger(string str, string keyword){
+	size_t index = 0;
+	int numLength =0;
+	int result =-1;
+    while (index < str.size())
+    {
+        if (isspace(str[index]))
+            index++;
+        else
+            break;
+    }
+	if (str.substr(index,keyword.size()+2)=="<"+keyword+">")
+		return 1;//triger start element
+	else if(str.substr(index,keyword.size()+3)=="</"+keyword+">")
+		return -1;//triger finish element
+	return 0;//default
+}
 bool XMLParser::parse(const char *filename, std::vector<GVersion> *Versions) 
 {
 	string line, targetString;
+	int result;
+	int counter=100;
 	string s="output";
 	ifstream file (s);
 	if (file.is_open())
@@ -71,12 +91,25 @@ bool XMLParser::parse(const char *filename, std::vector<GVersion> *Versions)
     while ( file.good() )
     {
       getline (file,line);
-
-
-	  targetString =getNumber(line,"FILEID");
-
-
-    }
+//	  if(line.substr(0,11).compare("<FILEID>")==0){
+//	  keyString =line
+	  if (flagTrigger(line,"FILE")==1){
+		  counter =1;
+		  cout<<"start file"<<endl;
+	  }
+	  else if(flagTrigger(line,"FILE")==0)
+		  counter ++;
+	  else if(flagTrigger(line,"FILE")==-1){
+		  counter ++;
+		  cout<<"end file"<<endl;
+	  }
+		  cout<<"i'm "<< counter<<endl;
+	  result =getNumber(line,"FILEID");
+//	  if( result !=-1)
+//	  cout<<result<<endl;
+	  //	  		  cout<<line<<endl;
+//		  cout<<getNumber(line, )<<endl;
+    };
 
     file.close();
 
